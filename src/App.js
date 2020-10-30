07305d540802f08683df1ec8d344dc4adf79cf35
MiRX <59041051+MRX710@users.noreply.github.com>
2020-10-30 22:05:41 +0300

    First edition

--- src/App.js
+++ src/App.js
@@ -17,3 +17,22 @@ const App = () => {
 }
 
 export default App;
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Header from "./Components/Header";
+import Nav from "./Components/Nav";
+import Profile from "./Components/Profile";
+
+
+const App = () => {
+    return (
+        <div className="wrapper">
+            <Header/>
+            <Nav/>
+            <Profile/>
+        </div>
+    );
+}
+
+export default App;
